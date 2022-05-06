ae6d8e98ec10d40cbfcebd73c54b58c54128dbff
SongYukGwon <thddbr1254@naver.com>
2022-05-06 20:29:44 +0900

    4_2

---
+++ 31Interface/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//사용자 정의 자료형
+interface QuestUnit
+{
+    //멤버변수가 안됨.
+
+
+    //함수의 형태만 물려 줄 수 있는 문법
+    //형태만 물려준다는 것은 선언만하고 정의를 하지 않는다는것.
+    //무조건 public
+    void Talk(QuestUnit _OtherUnit);
+
+    void Event(QuestUnit _OtherUnit);
+}
+
+interface QuestUnit2
+{
+    //멤버변수가 안됨.
+
+
+    //함수의 형태만 물려 줄 수 있는 문법
+    //형태만 물려준다는 것은 선언만하고 정의를 하지 않는다는것.
+    //무조건 public
+    void Talk(QuestUnit _OtherUnit);
+
+    void Event(QuestUnit _OtherUnit);
+}
+
+
+class FightUnit
+{
+    int AT;
+    int DMG;
+
+    public void Damage()
+    {
+
+    }
+}
+
+//인터페이스는 함수구현을 강제할 수 있다.
+//인터페이스는 상속이라고 안하는 사람도 있다.
+//포함의 개념이라고 볼수도 있다.
+//인터페이스를 상속받았다고 치면
+//
+class Player: FightUnit, QuestUnit, QuestUnit2
+{
+    public void Event(QuestUnit _OtherUnit)
+    {
+        
+    }
+
+    public void Talk(QuestUnit _OtherUnit)
+    {
+
+    }
+}
+
+class NPC : FightUnit, QuestUnit, QuestUnit2
+{
+    public void Event(QuestUnit _OtherUnit)
+    {
+        
+    }
+
+    public void Talk(QuestUnit _OtherUnit)
+    {
+
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player NewPlayer = new Player();
+        NPC NewNpc = new NPC();
+        //인터페이스는 객체생성 불가
+        //QuestUnit questUnit = new QuestUnit();
+
+        QuestUnit NewQuestUnit = NewNpc;
+
+
+        // 업캐스팅이 된다.
+        NewPlayer.Talk(NewNpc);
+        NewNpc.Talk(NewPlayer);
+    }
+}
+
---
+++ 32Array/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    class Item
+    {
+        public string Name;
+        public int AT;
+        public int DF;
+    }
+
+    static void Main(string[] args)
+    {
+        //배열은 기본적으로 자료형이라고 할 수 있으며
+        //기본자료형에 속한다.
+        // 선언 방식은 어떤 자료형 []
+
+
+        // 모든 자료형은 구조체 아니면 클래스에 가까운데,
+        // 기본 자료형도 마찬가지이다.
+
+        // [0][0][0][0][0][0][0][0][0][0]
+        // int가 10개 생겼다.
+        int[] ArrInt = new int[10];
+
+        //ArrInt는 int의 배열형
+        //ArrInt[0]은 무슨 자료형 ? int형
+
+        // c#에서는 모든 자료형은 클래스 혹은 구조체에 가깝다.
+        // -> 멤버변수와 멤버 함수가 있다.
+
+        //
+
+        for(int i = 0; i<ArrInt.Length; i++)
+        {
+            Console.WriteLine(ArrInt[i]);   
+        }
+        //배열을 언제 사용하느냐?
+        
+        //클래스도 자료형이 될 수 있다.
+        //기본자료형과는 다른게 아이템이라는 참조형을 담을수 있는 공간이 10개 생김
+        //아이템이라는 메모리를 가리킬 수 있는 참조형이 10개 생김
+        //선언 된것이 아님.
+        Item[] ArrItem = new Item[10];
+
+        for(int i =0; i<ArrItem.Length; i++)
+        {
+            //클래스는 배열을 일일이 new로 선언해줘야함.
+            ArrItem[i] = new Item();
+        }
+
+        ArrItem[0].Name = "철검";
+        ArrItem[1].Name = "전설의검";
+        ArrItem[2].Name = "멋진갑옷";
+        ArrItem[3].Name = "ㅇㅇㅇㅇ";
+
+
+        for(int i =0; i<ArrItem.Length; i++)
+        {
+            Console.WriteLine(ArrItem[i].Name);
+        }
+        // ************
+        // ************
+        // ************
+
+        
+
+    }
+}
+
